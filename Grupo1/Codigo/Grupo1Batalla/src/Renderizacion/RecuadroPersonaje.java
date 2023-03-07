425991dea262cdb3fbabca4c6900419acc0703a1
Dani <daniel.fernandez@alumnos.uneatlantico.es>
2023-03-07 14:25:49 +0100

    Re-hecho clases

--- Grupo1/Codigo/Grupo1Batalla/src/Renderizacion/RecuadroPersonaje.java
+++ Grupo1/Codigo/Grupo1Batalla/src/Renderizacion/RecuadroPersonaje.java
@@ -1,2 +1,13 @@
-package Renderizacion;public class RecuadroPersonaje {
+package Renderizacion;
+
+public class RecuadroPersonaje {
+    String[] sprite;
+    int vida;
+    String nombre;
+
+    public RecuadroPersonaje(String[] sprite){
+
+    }
+
+    public void
 }
