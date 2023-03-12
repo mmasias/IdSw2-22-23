3d02b5f3a9577e0849075facf4034ee30671a4c2
mauro soler <javiermauriciosoler@gmail.com>
2023-03-12 19:37:56 +0100

    feat class Tiempo added

--- entregas/Grupo4/Codigo/Ascensores/src/Tiempo.java
+++ entregas/Grupo4/Codigo/Ascensores/src/Tiempo.java
@@ -1,2 +1,33 @@
-public class Tiempo {
+import java.util.Date;
+
+class Tiempo {
+    private Date tiempoFinal;
+    private Date tiempoActual;
+
+    public Tiempo() {
+        tiempoActual = new Date();
+        tiempoFinal = null;
+    }
+
+    public Tiempo(Date tiempoFinal) {
+        this.tiempoFinal = tiempoFinal;
+        tiempoActual = new Date();
+    }
+
+    public void avanzar() {
+        tiempoActual.setTime(tiempoActual.getTime() + 1000);
+    }
+
+    public Date getTiempoActual() {
+        return tiempoActual;
+    }
+
+    public Date getTiempoFinal() {
+        return tiempoFinal;
+    }
+
+    public boolean isFinalizado() {
+        return tiempoActual.after(tiempoFinal);
+    }
+
 }
