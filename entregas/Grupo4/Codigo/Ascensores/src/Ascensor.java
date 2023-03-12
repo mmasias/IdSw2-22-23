790256b53a58a502ac52c20308c60ead011ca37f
hector.arrechea <hector.arrechea@ct.uneatlantico.es>
2023-03-12 19:37:01 +0100

    feat create logic class ascensor

--- entregas/Grupo4/Codigo/Ascensores/src/Ascensor.java
+++ entregas/Grupo4/Codigo/Ascensores/src/Ascensor.java
@@ -1,2 +1,42 @@
-public class Ascensor {
+import java.util.List;
+
+class Ascensor {
+
+    private Planta plantaActual;
+    private List<Boton> botones;
+
+    private List<Puerta> puertas;
+
+    public Ascensor(Planta plantaActual, Boton[] botones) {
+        this.plantaActual = plantaActual;
+        this.botones = List.of(botones);
+        this.puertas = null;
+    }
+
+    public void agregarDestino(Planta plantaDestino) {
+        int index = botones.indexOf(new Boton(plantaDestino.getNivel()));
+        botones.get(index).presionar();
+    }
+
+    public void agregarPuertas(Puerta[] puertas) {
+        this.puertas = List.of(puertas);
+    }
+
+    public void abrirPuerta(Boton boton) {
+        puertas.get(puertas.indexOf(new Puerta(boton))).abrir();
+    }
+
+    public void cerrarPuerta(Boton boton) {
+        puertas.get(puertas.indexOf(new Puerta(boton))).cerrar();
+    }
+
+    public Planta getPlantaActual() {
+        return plantaActual;
+    }
+
+    public void mover(Boton botonPulsado) {
+
+    }
+
+
 }
