755181051ea1517c74f5ac19bbe4bc0918bcfbc1
mauro soler <javiermauriciosoler@gmail.com>
2023-03-12 19:37:27 +0100

    feat: class Boton added

--- entregas/Grupo4/Codigo/Ascensores/src/Boton.java
+++ entregas/Grupo4/Codigo/Ascensores/src/Boton.java
@@ -1,2 +1,25 @@
-public class Boton {
+class Boton {
+    private int nivel;
+    private boolean presionado;
+
+    public Boton(int nivel) {
+        this.nivel = nivel;
+        presionado = false;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void presionar() {
+        presionado = true;
+    }
+
+    public void soltar() {
+        presionado = false;
+    }
+
+    public boolean estaPresionado() {
+        return presionado;
+    }
 }
