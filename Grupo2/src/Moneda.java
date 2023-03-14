b865ceb2ddab6a32920d4abe0eca8d008d2fdac6
OmarBarrios <momarbarrios863@gmail.com>
2023-03-14 23:44:01 +0100

    creacion de las 3 maquinas con sus billetes, monedas y productos

--- Grupo2/src/Moneda.java
+++ Grupo2/src/Moneda.java
@@ -1,52 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
 public class Moneda extends Dinero {
     Moneda(double valor, int cantidad) {
         super(valor, cantidad);
     }
 
-    public static List<Moneda> menu(Scanner scanner) {
-        try {
-            boolean salir = false;
-            List<Moneda> monedas = new ArrayList<Moneda>();
-            Scanner input = new Scanner(System.in);
-
-            do {
-                String option = "";
-                System.out.println("-----------------------------------------------------");
-                System.out.println("[1|si] Agregar monedas?");
-                System.out.println("[2] Salir");
-                System.out.println("----------------                   Elige una opcion: ");
-                option = input.nextLine();
-
-                if (option.equals("1"))
-                    agregar(input, monedas);
-                else if (option.equals("2")) {
-                    System.out.println("Saliendo...");
-                    salir = true;
-                }
-            } while (!salir);
-
-            return monedas;
-        } catch (Exception e) {
-            throw new Error(e.getMessage());
-        }
-    }
-
-    public static List<Moneda> agregar(Scanner scanner, List<Moneda> monedas) {
-        try {
-            System.out.println("=================================================================");
-            System.out.println("Valor: ");
-            double valor = scanner.nextDouble();
-            System.out.println("Cantidad: ");
-            int cantidad = scanner.nextInt();
-            monedas.add(new Moneda(valor, cantidad));
-            System.out.println("=================================================================");
-            return monedas;
-        } catch (Exception e) {
-            throw new Error(e.getMessage());
-        }
+    public static List<Moneda> crearListaDeMonedas(int cant1, int cant2, int cant3) {
+        List<Moneda> monedas = new ArrayList<Moneda>();
+        monedas.add(new Moneda(0.50, cant1));
+        monedas.add(new Moneda(0.20, cant2));
+        monedas.add(new Moneda(0.05, cant3));
+        return monedas;
     }
 }
--- Grupo2/src/Producto.java
+++ Grupo2/src/Producto.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
 public class Producto {
     String nombre;
     double precio;
@@ -25,48 +23,12 @@ public class Producto {
         this.cantidad = cantidad;
     }
 
-    public static List<Producto> menu(Scanner scanner) {
-        try {
-            boolean salir = false;
-            List<Producto> productos = new ArrayList<Producto>();
-            Scanner input = new Scanner(System.in);
-
-            do {
-                String option = "";
-                System.out.println("-----------------------------------------------------");
-                System.out.println("[1|si] Agregar productos?");
-                System.out.println("[2] Salir");
-                System.out.println("----------------                   Elige una opcion: ");
-                option = input.nextLine();
-
-                if (option.equals("1"))
-                    agregar(input, productos);
-                else if (option.equals("2")) {
-                    System.out.println("Saliendo...");
-                    salir = true;
-                }
-            } while (!salir);
-
-            return productos;
-        } catch (Exception e) {
-            throw new Error(e.getMessage());
-        }
-    }
-
-    public static List<Producto> agregar(Scanner scanner, List<Producto> productos) {
-        try {
-            System.out.println("=================================================================");
-            System.out.println("Nombre: ");
-            String nombre = scanner.nextLine();
-            System.out.println("Precio: ");
-            double precio = scanner.nextDouble();
-            System.out.println("Cantidad: ");
-            int cantidad = scanner.nextInt();
-            productos.add(new Producto(nombre, precio, cantidad));
-            System.out.println("=================================================================");
-            return productos;
-        } catch (Exception e) {
-            throw new Error(e.getMessage());
-        }
+    public static List<Producto> crearListaDeProductos() {
+        List<Producto> productos = new ArrayList<Producto>();
+        productos.add(new Producto("Galletas", 1.50, 10));
+        productos.add(new Producto("Chocolates", 1.10, 10));
+        productos.add(new Producto("Bebida", 1.05, 10));
+        productos.add(new Producto("Bocadillo", 1.75, 10));
+        return productos;
     }
 }
