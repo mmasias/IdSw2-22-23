2e69ae8cc54253752daced0604604a61e6f23551
Manuel Ortega <maopmore@gmail.com>
2023-06-09 01:05:58 +0200

    agregar sobrecarga al constructor

--- Grupo1/V2/Codigo/Grupo1Batalla/src/configuracionPjs/CreadorPersonajes.java
+++ Grupo1/V2/Codigo/Grupo1Batalla/src/configuracionPjs/CreadorPersonajes.java
@@ -5,9 +5,9 @@ import objetos.*;
 public class CreadorPersonajes {
 
     public static Enemigo Enemigo(String nombre){
-        Arma arma1Enemigo = new Arma("Mercadona", 5, 15, 90, null);
-        Arma arma2Enemigo = new Arma("Beso de gato", 10, 15, 60, null);
-        Arma arma3Enemigo = new Arma("TEO", 20, 10, 40, null);
+        Arma arma1Enemigo = new Arma("Mercadona", 5, 15, 100);
+        Arma arma2Enemigo = new Arma("Beso de gato", 10, 15, 80);
+        Arma arma3Enemigo = new Arma("TEO", 20, 10, 70);
 
         Arma[] armasEnemigo = new Arma[]{arma1Enemigo, arma2Enemigo, arma3Enemigo};
 
@@ -29,17 +29,16 @@ public class CreadorPersonajes {
         };
 
         Vida vida = new Vida(60, 20,2);
-        Enemigo enemigo = new Enemigo(nombre, vida, armasEnemigo, spriteEnemigo);
 
-        return enemigo;
+        return new Enemigo(nombre, vida, armasEnemigo, spriteEnemigo);
 
     }
 
     public static Heroe Heroe(String nombre){
-        Arma arma1Heroe = new Arma("Thousand truths", 7, 100, 50, null);
-        Arma arma2Heroe = new Arma("Excalibur", 45, 15, 100, null);
-        Arma arma3Heroe = new Arma("Hamburguesa", 25, 10, 12, null);
-        Arma arma4Heroe = new Arma("Flora", 10, 5, 50, new EfectoAturdir());
+        Arma arma1Heroe = new Arma("Thousand truths", 7, 100, 100);
+        Arma arma2Heroe = new Arma("Excalibur", 45, 15, 80);
+        Arma arma3Heroe = new Arma("Hamburguesa", 25, 10, 90);
+        Arma arma4Heroe = new Arma("Flora", 10, 5, 80, new EfectoAturdir());
 
         Arma[] armasHeroe = new Arma[]{arma1Heroe, arma2Heroe, arma3Heroe, arma4Heroe};
 
@@ -67,8 +66,7 @@ public class CreadorPersonajes {
 
 
         Vida vida = new Vida(150, 30, 2);
-        Heroe heroe = new Heroe(nombre, vida, armasHeroe, spriteHeroe);
 
-        return heroe;
+        return new Heroe(nombre, vida, armasHeroe, spriteHeroe);
     }
 }
