9833e3bc97066ab0e0e6274db99fc9e430bf08ad
IvanSantoveniaMantecon <102520821+IvanSantoveniaMantecon@users.noreply.github.com>
2023-06-08 10:46:35 +0200

    Update Arena.java

--- src/es/laberinto/modelo/bloques/Arena.java
+++ src/es/laberinto/modelo/bloques/Arena.java
@@ -2,6 +2,7 @@ package es.laberinto.modelo.bloques;
 
 import es.laberinto.controlador.Mundo;
 import es.laberinto.modelo.entides.Alfombra;
+import es.laberinto.modelo.entides.Barca;
 import es.laberinto.modelo.entides.Entidad;
 import es.laberinto.modelo.entides.Personaje;
 import es.laberinto.vista.SePuedeRenderizar;
@@ -14,7 +15,7 @@ public final class Arena extends Bloque implements SePuedeRenderizar {
 
     @Override
     public boolean puedeTransitar(Entidad entidad) {
-        return entidad.getClass() == Personaje.class || entidad.getClass() == Alfombra.class;
+        return entidad.getClass() == Personaje.class || entidad.getClass() == Alfombra.class || entidad.getClass() == Barca.class;
     }
 
     @Override
